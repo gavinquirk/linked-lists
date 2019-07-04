dc4356ebe91d0b96778086701de221e61de03faa
Gavin Quirk <gavinquirk@gmail.com>
2019-07-03 17:40:55 -0700

    insertAt method

--- index.js
+++ index.js
@@ -41,6 +41,36 @@ class LinkedList {
   }
 
   // Insert at index
+  insertAt(data, index) {
+    // If index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // If first index
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
 
   // Get at index
 
@@ -64,5 +94,6 @@ ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
 ll.insertLast(400);
+ll.insertAt(500, 10);
 
 ll.printListData();
